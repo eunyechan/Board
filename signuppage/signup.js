f5c79473050bbdf6bf473fb9909713732f7892a0
yechan <ys101312@gmail.com>
2021-03-01 18:57:36 +0900

    commitPhp

---
+++ signuppage/signup.js
@@ -0,0 +1,18 @@
+function signup(){
+  const id=document.getElementById('id').value;
+  const pw=document.getElementById('pw').value;
+  const email=document.getElementById('email').value;
+
+  
+      var req=new XMLHttpRequest();
+      req.onreadystatechange=function(){
+          if(req.readyState==4&&req.status==200){
+              alert(req.responseText);
+              location.href="./signup.html"
+          }
+                  
+      req.open('POST', './signup.php', true);
+      req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+      req.send('id='+id+'&pw='+pw+'&email='+email);  
+  }
+}
---
+++ signuppage/signup.php
@@ -0,0 +1,26 @@
+<?php
+
+  header('Content-Type:text/html; charset=utf-8');
+
+  $id=$_POST['id'];
+  $pw=$_POST['pw'];
+  $email=$_POST['email'];
+
+  $conn=mysqli_connect("localhost", "eunyechan", "hshong1106!", "eunyechan");
+
+  mysqli_query($conn, 'set names utf-8');
+
+  $sql="INSERT INTO `users` (id, pw, email) VALUES ('$id', '$pw', '$email')";
+  $result=mysqli_query($conn, $sql);
+
+  if($result){
+    echo '저장됐습니다';
+  }else{
+    echo '에러';
+  }
+
+  mysqli_close($conn);
+
+
+
+?>
